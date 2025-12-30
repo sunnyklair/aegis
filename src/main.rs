40d9ddebcc2b9fbdb6277cd054b3cfde457ca6a9
sunny klair <sk@sk.wtf>
2025-12-29 23:21:17 -0500

    Add bevy_dev_tools integration for enhanced debugging
    - Updated Cargo.toml to include bevy_dev_tools as a feature for the Bevy dependency.
    - Modified main.rs to incorporate FpsOverlayPlugin for displaying FPS and frame time graph in debug builds.
    - Improved application setup by organizing the addition of plugins and systems for better clarity.

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,37 @@ enum GameState {
 }
 
 fn main() {
-    App::new()
-        .add_systems(Startup, setup)
+    let mut app = App::new();
+
+     app.add_systems(Startup, setup)
         .add_plugins((DefaultPlugins, MenuPlugin))
-        .init_state::<GameState>()
-        .run();
+        .init_state::<GameState>();
+
+    #[cfg(debug_assertions)] { // Debug or dev builds only
+        use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig};
+        use bevy::text::{TextFont, FontSmoothing};
+
+        app.add_plugins(FpsOverlayPlugin {
+            config: FpsOverlayConfig {
+                text_config: TextFont {
+                    font_size: 24.0,
+                    font: default(),
+                    font_smoothing: FontSmoothing::default(),
+                    ..default()
+                },
+                text_color: Color::srgb(0.0, 1.0, 0.0),
+                refresh_interval: core::time::Duration::from_millis(100),
+                enabled: true,
+                frame_time_graph_config: FrameTimeGraphConfig {
+                    enabled: true,
+                    min_fps: 30.0,
+                    target_fps: 60.0,
+                },
+            },
+        });
+    }
+
+    app.run();
 }
 
 fn setup(mut commands: Commands) {
